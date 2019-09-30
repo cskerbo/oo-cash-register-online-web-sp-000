ba410802c7257c48c830402e40e473fe36dbae5f
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-30 23:05:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
 
-  attr_accessor :total
+  attr_accessor :total, :discount
 
   def initialize(discount)
-    @total = discount - 0
+    @total = 0
+    @discount = discount
   end
 
 end
