560c1e5b1b70a96a7dae7ec8f7afd840f2c9d72a
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-01 00:33:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     @title = title
     @price = price
     @quantity = quantity
-    @@item_list.concat([@title]*@quantity)
+    @@item_list.fill([@title]*@quantity)
     if @quantity == 0
       @total += @price
     else
